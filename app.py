861f989af24721e217b3a0ba3e647d23baccd101
Lookforrachel <lookfor.rachel@gmail.com>
2022-04-25 17:44:09 -0400

    Variables & data types

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-# Order of code matters
-print("    /|")
-print("   / |")
-print("  /  |")
-print(" /   |")
-print("/____|")
+# Using variables in print lines
+character_name = "Bleep"
+character_age = 5
+is_male = True
+print("My name is " + character_name + "and I am " + str(character_age) + " years old")
+character_name = "Bleepo"
+print("When I get older I will tell people to call me " + character_name)
