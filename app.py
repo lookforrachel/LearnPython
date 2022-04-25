a72b5e337c596fd372b2972e51015c2090f43540
Lookforrachel <lookfor.rachel@gmail.com>
2022-04-25 18:25:51 -0400

    Working with strings

--- app.py
+++ app.py
@@ -1,7 +1,31 @@
-# Using variables in print lines
-character_name = "Bleep"
-character_age = 5
-is_male = True
-print("My name is " + character_name + "and I am " + str(character_age) + " years old")
-character_name = "Bleepo"
-print("When I get older I will tell people to call me " + character_name)
+# Working with strings
+
+phrase = "I said: "
+
+# New line
+print("Hello \nworld")
+
+# Inserting quotation marks in strings
+print("Hello \"world\"")
+
+# Concatenation
+print(phrase + "Hello world")
+
+# upper case / lower case
+print(phrase.lower())
+print(phrase.upper())
+print(phrase.isupper())
+print(phrase.upper().isupper())
+
+# length of string
+print(len(phrase))
+
+# first letter of phrase
+print(phrase[0])
+
+# first index of ___
+print(phrase.index("a"))
+print(phrase.index("said"))
+
+# replacing words
+print(phrase.replace("said", "shouted"))
